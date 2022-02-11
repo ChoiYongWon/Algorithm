b796eb03727df80160eb82ee1c297a2ca51070a9
ChoiYongWon <yongwon0824@naver.com>
2022-02-11 19:44:24 +0900

    Level 3 베스트앨범 해결

---
+++ Programmers/Level3/베스트앨범.js
@@ -0,0 +1,36 @@
+function solution(genres, plays) {
+    var answer = [];
+    let genre = {}
+    let result = []
+    
+    for(let i=0;i<genres.length;i++){
+        
+        genre[genres[i]] = [...genre[genres[i]] || [], {
+            id: i,
+            plays: plays[i]
+        }]
+    }
+    for(let i in genre){
+        genre[i] = genre[i].sort((a, b)=>{
+            return b.plays - a.plays
+        })
+        answer.push({
+            genre: i,
+            musics: genre[i]
+        })
+    }
+
+    answer = answer.sort((a, b)=>{
+        return b.musics.reduce((acc, cur)=>acc+cur.plays, 0) - a.musics.reduce((acc, cur)=>acc+cur.plays, 0)
+    })
+    
+    for(let i=0;i<answer.length;i++){
+        if(answer[i].musics.length < 2) result.push(answer[i].musics[0].id)
+        else{
+            result.push(answer[i].musics[0].id)
+            result.push(answer[i].musics[1].id)
+        }
+    }
+    
+    return result;
+}
