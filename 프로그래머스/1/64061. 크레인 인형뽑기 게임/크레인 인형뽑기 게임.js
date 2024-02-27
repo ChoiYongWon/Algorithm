974912bd6e5faf8cbd8347c1af5113001650ab8e
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-02-28 01:57:51 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.02 ms, Memory: 30.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,20 @@
+function solution(board, moves) {
+    var answer = 0;
+    let basket = []
+    for(let i=0;i<moves.length;i++){
+        for(let j=0;j<board.length;j++){
+            let doll = board[j][moves[i]-1]
+            if(doll!=0){
+                board[j][moves[i]-1] = 0
+                basket.push(doll)
+                break
+            }
+            
+        }
+        if(basket.length > 1 && basket[basket.length-1] == basket[basket.length-2]){
+            basket.splice(-2, 2)
+            answer += 2
+        }
+    }
+    return answer;
+}
