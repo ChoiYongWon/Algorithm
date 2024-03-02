1e1e2ecc8910c6ff5ec7a2d99d709eada0aff48f
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-02 20:44:43 +0900

    [level 1] Title: 옹알이 (2), Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,7 +1,7 @@
 function solution(babbling) {
     var answer = 0;
     
-    const regexp = /((aya)|(ye)|(woo)|(ma))(?!\1)/g
+    const regexp = /((aya)|(ye)|(woo)|(ma))(?!\1){1,}/g
     answer = babbling.reduce((acc, cur)=>{
         
         if(cur.replace(regexp, '').length == 0) acc+=1
