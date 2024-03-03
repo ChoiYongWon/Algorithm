cd408ceaad99759005397176c609f9fa1f806a54
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-03 17:20:44 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.60 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,9 +1,9 @@
 function solution(board, moves) {
     var answer = 0;
     let basket = []
-    for(let i=0;i<moves.length;i++){
-        for(let j=0;j<board.length;j++){
-            let doll = board[j][moves[i]-1]
+    for(let i=0;i<moves.length;i++){ // 가변
+        for(let j=0;j<board.length;j++){ //5번, 내려오면서 존재하는 인형 감지
+            let doll = board[j][moves[i]-1] //보드에서는 index가 1부터 시작
             if(doll!=0){
                 board[j][moves[i]-1] = 0
                 basket.push(doll)
@@ -11,6 +11,7 @@ function solution(board, moves) {
             }
             
         }
+        // 연속되는 인형 감지
         if(basket.length > 1 && basket[basket.length-1] == basket[basket.length-2]){
             basket.splice(-2, 2)
             answer += 2
