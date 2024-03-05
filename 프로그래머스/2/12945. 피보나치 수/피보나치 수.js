c2a4909252bee652cbe9623fc6f6439730aba973
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-05 15:39:07 +0900

    [level 2] Title: 피보나치 수, Time: 5.97 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,13 @@
-
+// 재귀함수로 짰다가 콜스택 줫나게 쌓여서 for문으로 바꿈
+// pool 객체에 n 값에 대한 결과를 계속 저장
+// 중간에 1234567로 안나눠주면 Infinity로 바뀜
 
 function solution(n) {
     var answer = 0;
     const pool = {0:0, 1:1}
     for(let i=2; i<=n; i++){
-        pool[i] = (pool[i-1] + pool[i-2])% 1234567
+        pool[i] = (pool[i-1] + pool[i-2]) % 1234567
     }
-    console.log(n, pool[n])
     answer = pool[n] 
     return answer;
 }
