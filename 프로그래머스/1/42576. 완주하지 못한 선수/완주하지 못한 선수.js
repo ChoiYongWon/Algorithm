52008185b311a65616c38c745db0d041a2988fb6
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-05 17:47:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 104.68 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,21 @@
 // 해시는 거의 객체 사용
 
 function solution(participant, completion) {
-    
+
     let players = {}
     let answer = ""
-    
+
     // 참여자를 객체에 추가
     participant.forEach((name) => {
         if(players[name]==undefined) players[name] = 1
         else players[name] += 1
     });
-    
+
     // 완주자 객체에서 제거
     completion.forEach((name) => {
         players[name] -= 1
     })
-    
+
     // 객체에 남은 플레이어 중 0이 아닌 플레이어 추출
     players = Object.entries(players).filter(info=>info[1]!=0).map(info=>info[0])    
     answer = players.join("")
