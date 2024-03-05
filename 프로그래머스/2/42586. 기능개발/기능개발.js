4543a6e05c71900831d55c20f54106daa2172c44
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-05 19:42:16 +0900

    [level 2] Title: 기능개발, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,4 +1,24 @@
 function solution(progresses, speeds) {
     var answer = [];
+    let progress = [...progresses]
+    let speed = [...speeds]
+    
+    
+    while(progress.length > 0){
+        // 진행률을 진행속도 만큼 더함
+        for(let i in progress) progress[i] += speed[i] 
+        
+        let count = 0;
+        
+        // 첫번째 진행률이 100이 크면 계속 뺴줌
+        while(progress[0] >= 100){
+            progress.shift()
+            speed.shift()
+            count ++
+        }
+        if(count > 0) answer.push(count)
+        
+    }
+    
     return answer;
 }
