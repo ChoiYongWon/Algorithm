99a001deb6770ae16ccb3f3cfcce50401c2c04b8
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-05 16:57:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,20 +1,19 @@
 function solution(n, words) {
     var answer = [];
-    let bank = []
-    let last_word = ""
-    let i
-    for(i=0;i<words.length;i++){
-        
-        if(i>0 && words[i][0]!=last_word[last_word.length-1]) break 
-        if(words[i].length == 1) break 
-        if(bank.includes(words[i])) break
-        
-        bank.push(words[i])
-        last_word = words[i]
 
+    for(let i in words){
+        const person = (i%n) + 1
+        const turn = Math.floor(i/n) + 1
+        // 한 글자 검증 (첫사람도 검증해야됨)
+        if(words[i].length == 1) return [person, turn] 
+        if(i==0) continue //젤 첫사람 턴
+        
+        // 문자 일치 검증
+        if(words[i-1][words[i-1].length-1] != words[i][0]) return [person, turn]
+        // 사용된 문자 검증
+        if(words.slice(0, i).includes(words[i])) return [person, turn]
     }
-    if(i==words.length) answer = [0,0]
-    else answer = [i%n + 1, Math.floor(i/n)+1]
     
+    answer = [0,0]
     return answer;
 }
