5cf121be4b9986d718be2a1894b824c6f3659277
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-09 14:45:38 +0900

    [level 2] Title: 예상 대진표, Time: 2.56 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n,a,b)
+{
+    let [min,max] = [Math.min(a,b), Math.max(a,b)]
+    let bin = n
+    let fivot = bin
+    console.log(min, max)
+    do{
+        bin = bin >> 1
+        if(min > fivot) fivot += bin
+        else if(max <= fivot) fivot -= bin
+        else return bin.toString(2).split("").reverse().join("").indexOf("1") + 2
+    }while(1)
+}
