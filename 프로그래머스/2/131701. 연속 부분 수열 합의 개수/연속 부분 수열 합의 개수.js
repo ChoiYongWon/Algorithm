cf2a65921bb5a46b8d9a81d32f32305504628ac9
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-09 16:45:22 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2620.00 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,6 +1,6 @@
 function solution(elements) {
     var answer = new Set();
-    for(let i=1;i<=elements.length;i++){
+    for(let i=1;i<elements.length;i++){
         for(let j=0;j<elements.length;j++){
             let sum = 0;
             for(let k=0;k<i;k++){
@@ -9,5 +9,5 @@ function solution(elements) {
             answer.add(sum)
         }
     }
-    return Array.from(answer).length
+    return Array.from(answer).length + 1
 }
