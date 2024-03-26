beea64d2f697752841333816d9b6d83dc65332e9
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-26 10:40:05 +0900

    [level 3] Title: 입국심사, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#qna) 
 
 ### 성능 요약
 
-메모리: 30.4 MB, 시간: 0.12 ms
+메모리: 33.4 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 09:44:14
+2024년 03월 26일 10:40:03
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,16 +1,16 @@
 function solution(n, times) {
     var answer = 0;
-    let maxTime = Math.floor(Math.max.apply(null, times) * n / times.length)
-    let minTime = Math.floor(Math.min.apply(null, times) * n / times.length)
+    let maxTime = Math.floor(Math.max.apply(null, times) * n / times.length) // 최소값
+    let minTime = Math.floor(Math.min.apply(null, times) * n / times.length) // 최대값
     while(minTime <= maxTime){
         
-        let mid = Math.floor((maxTime+minTime)/2)
-        let count = times.reduce((acc, cur)=> acc + Math.floor((mid/cur)), 0)
+        let mid = Math.floor((maxTime+minTime)/2) // 중간 값
+        let count = times.reduce((acc, cur)=> acc + Math.floor((mid/cur)), 0) // 해당 분 동안 심사관이 처리한 인원 수
         
-        if(n<=count){
+        if(n<=count){ // 처리한 인원이 더 많으면 더 낮은 값 찾도록 -1
             maxTime = mid - 1
         }
-        else{
+        else{ // 처리한 인원이 더 적으면 더 높은 값 찾도록 +1
             minTime = mid + 1
         }
     }
