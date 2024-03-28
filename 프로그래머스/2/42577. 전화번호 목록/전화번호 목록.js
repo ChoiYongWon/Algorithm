420e7309da35a4819591998c7a6f74fe9e972c29
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-03-29 01:47:30 +0900

    [level 2] Title: 전화번호 목록, Time: 1867.19 ms, Memory: 327 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -3,6 +3,10 @@ function solution(phone_book) {
     let map = new Map();
     // const min_length = Math.min(...phone_book.map(i=>i.length))
     for(let i in phone_book){
+        // 이거 왜안됨??
+        // for(let j=0;j<phone_book[i].length-min_length;j++){
+        //     map.set(phone_book[i].slice(0, min_length+j), true)
+        // }
         for(let j=1;j<phone_book[i].length;j++){
             map.set(phone_book[i].slice(0, j), true)
         }
