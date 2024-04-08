8583a761c005519588eff3cf70730bf5e0a4b384
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-04-08 10:33:36 +0900

    [level 3] Title: 단속카메라, Time: 31.27 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,10 +1,15 @@
 function solution(routes) {
     var answer = 0;
+    
+    // 진출점을 기준으로 정렬
     let sorted_routes = routes.sort((a,b)=>a[1]-b[1])
     
     let fivot = -30001;
+    
         
     for(let i=0;i<sorted_routes.length;i++){
+        
+        // 진입점이 fivot보다 크면 진출점으로 fivot을 바꿈
         if(sorted_routes[i][0] > fivot) {
             fivot = sorted_routes[i][1];
             answer++
