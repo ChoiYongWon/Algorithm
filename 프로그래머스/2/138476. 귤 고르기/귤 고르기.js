a6d5c92de7cbf20b78a2107ec9aacf0b01c8b6bd
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-04-11 19:37:10 +0900

    [level 2] Title: 귤 고르기, Time: 112.66 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,14 +1,27 @@
 function solution(k, tangerine) {
-    var answer = {};
+    var answer = new Set();
     let sortedTangerine = []
+    // 귤 : 갯수
     let tanObj = tangerine.reduce((acc, cur)=>{
         acc[cur] = (acc[cur] || 0) + 1
         return acc
     }, {})
     tanObj = Object.entries(tanObj)
+    
+    // 귤 갯수로 정렬
     tanObj = tanObj.sort((a, b)=>(b[1] - a[1]))
     
+    // 귤 평탄화 작업
+    /*
+    before
+    {
+        2: 3,
+        3: 2
+    }
     
+    after
+    [2, 2, 2, 3, 3]
+    */
     for(let i=0;i<tanObj.length;i++){
         for(let j=0;j<tanObj[i][1];j++){
             sortedTangerine.push(tanObj[i][0])
@@ -16,8 +29,9 @@ function solution(k, tangerine) {
     }
     
     
+    // k만큼 더함 (set 중복방지)
     for(let i=0;i<k;i++){
-        answer[sortedTangerine[i]] = (answer[sortedTangerine[i]] || 0) + 1
+        answer.add(sortedTangerine[i])
     }
-    return Object.keys(answer).length;
+    return answer.size;
 }
