a014dc728a44140f58e43890293b3e07a94e9d61
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-04-11 17:52:40 +0900

    [level 2] Title: [1차] 캐시, Time: 1.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,22 +1,22 @@
 function solution(cacheSize, cities) {
     var answer = 0;
-    const cache = []
+    const cache = [] // 뒤로 갈수록 fresh한 데이터
     
     if(cacheSize == 0) return cities.length * 5
     
     for(let i=0;i<cities.length;i++){
         const city = cities[i].toLowerCase()
-        const cache_index = cache.indexOf(city)
+        const cache_index = cache.indexOf(city) // 캐싱되어있는 index
         // HIT
         if(cache_index!=-1){
-            cache.splice(cache_index, 1)
+            cache.splice(cache_index, 1)// 기존 캐시 위치 fresh
             cache.push(city)
             answer++
         }else{ // NO HIT
             if(cache.length == cacheSize){
-                cache.shift();
+                cache.shift(); // 맨 앞에꺼 제거
             }
-            cache.push(city)
+            cache.push(city) // 맨 뒤에 추가
             answer += 5
         }
     }    
