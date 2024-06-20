a88cf76aabf3a6841e41f82c4e88796bfe2aa204
ChoiYongWon <40623433+ChoiYongWon@users.noreply.github.com>
2024-06-20 18:33:27 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 5216 ms, Memory: 517044 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.js
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.js
@@ -3,10 +3,8 @@ let v_size = Number(input.shift())
 input = input.map(row=>row.split(" ").map(Number))
 
 let friend = Array.from({length:v_size+1}, ()=>[]) // 친구 관계
-let dp = Array.from({length:v_size+1}, ()=>[1, 0])
-let visited = Array.from({length:v_size+1}, ()=>0)
 
-
+// 초기화
 for(let i=0;i<input.length;i++){
     const from = input[i][0]
     const to = input[i][1]
@@ -15,17 +13,29 @@ for(let i=0;i<input.length;i++){
     friend[to].push(from)
 }
 
-function dfs(v){
-    visited[v] = true
-    for(let i=0;i<friend[v].length;i++){
-        const next = friend[v][i]
-        if(!visited[next]) { // 이전 노드가 아닐 시
-            dfs(next)
-            dp[v][1] += dp[next][0]; // 얼리 어답터가 아닐 경우 무조건 자식은 얼리 어답터
-            dp[v][0] += Math.min(dp[next][0], dp[next][1]) // 얼리 어답터일 경우 조건부
+// 재사용성 고려
+function find_lowest_ea(friend, v){
+
+    let visited = Array.from({length:v_size+1}, ()=>0)
+    let dp = Array.from({length:v_size+1}, ()=>[1, 0])
+
+    function dfs(v){
+        visited[v] = true
+        for(let i=0;i<friend[v].length;i++){
+            const next = friend[v][i] // 자식
+            if(!visited[next]) { // 방문 확인
+                dfs(next)
+                dp[v][1] += dp[next][0]; // 얼리 어답터가 아닐 경우 무조건 자식은 얼리 어답터
+                dp[v][0] += Math.min(dp[next][0], dp[next][1]) // 얼리 어답터일 경우 조건부
+            }
         }
     }
+
+    dfs(v, v)
+
+    return Math.min(dp[1][0], dp[1][1])
 }
 
-dfs(1, 1)
-console.log(Math.min(dp[1][0], dp[1][1]))
+
+let answer = find_lowest_ea(friend, 1)
+console.log(answer)
